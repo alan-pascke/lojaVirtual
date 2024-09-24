c8ac05985b9d264fa490c4cd12683656965389cb
alan-pascke <alan.pascke@gmail.com>
2024-09-24 02:05:43 -0300

    config docker melhorada

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,14 +3,26 @@
 // import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuTrigger } from "./ui/dropdown-menu";
 
 
 export default function Navbar() {
 
     return(
         <div className="flex h-14 border justify-center">
-            <div className="container flex w-full justify-end border">
-
+            <div className="container flex items-center w-full border">
+                <Link href={'/home'} className="">
+                    HOME    
+                </Link>
+                {/* <div className='relative border'>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger>Perfil</DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuLabel>Menu</DropdownMenuLabel>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div> */}
             </div>
         </div>
     )
