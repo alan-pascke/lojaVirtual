cf4a6ea05255fc30262cd66f1726aeae429c42f3
alan-pascke <alan.pascke@gmail.com>
2024-11-07 18:27:32 -0300

    teste

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 const port = 5000; 
  
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'https://themusicstore.vercel.app/',
     credentials: true,
 }
 
